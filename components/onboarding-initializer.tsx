045c65f70841f002dc7f7c810a0d4eed88f7f1a6
kolamarin <kolamarin92@gmail.com>
2025-08-24 20:11:22 +0100

    changes

--- components/onboarding-initializer.tsx
+++ components/onboarding-initializer.tsx
@@ -1,11 +1,11 @@
 "use client"
 
 import React, { useState, useEffect } from "react"
-import { OnboardingFlow } from "@/components/onboarding-flow"
+import { OnboardingModal } from "@/components/onboarding-modal"
 import { useOnboardingStore } from "@/lib/onboarding-store"
 
 export function OnboardingInitializer() {
-  const { hasSeenOnboarding, setHasSeenOnboarding, setCurrentStep } = useOnboardingStore()
+  const { hasSeenOnboarding } = useOnboardingStore()
   const [showOnboarding, setShowOnboarding] = useState(false)
   const [isLoaded, setIsLoaded] = useState(false)
 
@@ -21,25 +21,16 @@ export function OnboardingInitializer() {
     return () => clearTimeout(timer)
   }, [hasSeenOnboarding])
 
-  const handleComplete = () => {
-    setHasSeenOnboarding(true)
-    setCurrentStep(1) // Reset for next time
-    setShowOnboarding(false)
-  }
-
   // Don't render anything until we've loaded the store state
   if (!isLoaded) {
     return null
   }
 
-  // If onboarding should be shown, render it as a full-screen overlay
-  if (showOnboarding) {
-    return (
-      <div className="fixed inset-0 z-[9999] bg-gradient-to-b from-blue-50 to-white">
-        <OnboardingFlow onComplete={handleComplete} />
-      </div>
-    )
-  }
-
-  return null
+  // If onboarding should be shown, render it as a modal
+  return (
+    <OnboardingModal
+      open={showOnboarding}
+      onOpenChange={setShowOnboarding}
+    />
+  )
 }
